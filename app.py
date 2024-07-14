a4a09a20fbdea1a664c1c0d5801348962efd9511
Vivek <130243666+vivdevchikam@users.noreply.github.com>
2024-07-14 21:04:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,19 @@ import joblib
 app = Flask(__name__)
 
 data = pd.read_csv('Bangalorehousingprice.csv')
+delhid = pd.read_csv('Delhihousingprice.csv')
+
 
 model = joblib.load('XGBRegressor.joblib')
+modeld = joblib.load('LinearRegressor.joblib')
 
 @app.route('/')
 def index():
   location = sorted(data.location.unique())
   location.insert(0, ' ')
   return render_template('index.html', locations=location)
+
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -27,6 +32,30 @@ def predict():
   input = pd.DataFrame([[location, sqft, bathroom, bhk, pricesqft]], columns=['location', 'total_sqft', 'bath', 'bhk', 'price_per_sqft'])
   prediction = model.predict(input)[0] * 10000
   return str(np.round(prediction, 2))
+
+
+@app.route("/delhi")
+def delhi():
+  locality = sorted(delhid.Locality.unique())
+  locality.insert(0, ' ')
+  furnishing = sorted(delhid.Furnishing.unique())
+  furnishing.insert(0, ' ')
+  return render_template('delhi.html', localities=locality, furnishies=furnishing)
+
+
+@app.route('/predictdelhi', methods=['POST'])
+def predictdelhi():
+  locality = request.form.get('locality')
+  furnishing = request.form.get('furnishing')
+  bathroom = request.form.get('bathroom')
+  bhk = request.form.get('bhk')
+  area = request.form.get('area')
+  print(locality, furnishing, bathroom, bhk, area)
+  input = pd.DataFrame([[area, bhk, bathroom, furnishing, locality]], columns=['Area', 'BHK', 'Bathroom', 'Furnishing', 'Locality'])
+  prediction = modeld.predict(input)[0]
+  return str(np.round(prediction), 1)
+  
+
 
 
 if __name__ == "__main__":
